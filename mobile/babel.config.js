da98387fd3e0d706a8225c9a26991228a77646cb
phantom-patrick <anton11patrushev@gmail.com>
2021-04-24 20:58:27 +0300

    Enable TS path aliases

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset']
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          root: './',
+          app: ['./src/app'],
+          auth: ['./src/auth'],
+          chat: ['./src/chat'],
+          common: ['./src/common'],
+          settings: ['./src/settings'],
+        },
+      },
+    ],
+  ],
 };
