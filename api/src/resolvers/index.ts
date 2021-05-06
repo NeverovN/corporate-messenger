676df5c7b8f0f7a2ee572fd8869a1c6da2f862c0
phantom-patrick <anton11patrushev@gmail.com>
2021-05-06 22:53:03 +0300

    implement api codegen

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,9 +1,9 @@
-import { IResolvers } from 'graphql-tools';
+import { User, Resolvers } from '../types/gql.generated';
 
-const resolverMap: IResolvers = {
+const resolverMap: Resolvers = {
   Query: {
-    getUserByGuid(_: void, _args: { guid: string }): string {
-      return `👋 Hello world! 👋`;
+    getUserByGuid(_, args): User {
+      return { guid: args.guid, firstName: 'FirstName', lastName: 'LastName' };
     },
   },
 };
