1b2a989ef219414911a4edbd9a5bb3a9b182d496
NeverovN <neverovvnn@gmail.com>
2021-05-25 19:36:06 +0300

    api untrached changes from prev commit. query and gql resolvers setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { createServer } from 'http';
 import compression from 'compression';
 import schema from './schema';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { User } from './types/typeorm';
 
 const app = express();
 const server = new ApolloServer({
@@ -10,13 +12,28 @@ const server = new ApolloServer({
 });
 
 app.use(compression());
+app.listen(3000);
+console.log(
+  `\n🚀      GraphQL is now running on http://localhost:3000/graphql`,
+);
+
+createConnection({
+  type: 'mongodb',
+  host: 'localhost',
+  url:
+    'mongodb+srv://cirkul:gncirkul14@messengercluster.zam9m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  port: 3000,
+  username: 'cirkul',
+  password: 'gncirkul14',
+  database: 'test',
+  entities: [User],
+  synchronize: true,
+  logging: false,
+  useUnifiedTopology: true,
+})
+  .then(async (connection) => {
+    await connection.driver.afterConnect().then();
+  })
+  .catch((error) => console.log(error));
 
 server.applyMiddleware({ app, path: '/graphql' });
-
-const httpServer = createServer(app);
-
-httpServer.listen({ port: 3000 }, (): void =>
-  console.log(
-    `\n🚀      GraphQL is now running on http://localhost:3000/graphql`,
-  ),
-);
--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,9 +1,72 @@
-import { User, Resolvers } from '../types/gql.generated';
+import { Resolvers } from '../types/gql.generated';
+import { User } from '../types/typeorm';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// token
+import { ACCESS_SECRET_TOKEN } from '../consts/tokens';
 
 const resolverMap: Resolvers = {
   Query: {
-    getUserByGuid(_, args): User {
-      return { guid: args.guid, firstName: 'FirstName', lastName: 'LastName' };
+    async getUser(_, args) {
+      const verifiedUser = jwt.verify(
+        args.token,
+        ACCESS_SECRET_TOKEN,
+      ) as User | null;
+
+      console.log(verifiedUser);
+
+      return verifiedUser;
+    },
+    async getUsers() {
+      const users = await User.find();
+      console.log(users);
+      return users;
+    },
+    async getUserToken(_, args) {
+      const user = await User.findOne({
+        username: args.username,
+      });
+
+      if (typeof user === 'undefined') return null;
+
+      try {
+        if (await bcrypt.compare(args.password, user.password)) {
+          const encryptedUser = {
+            username: user.username,
+            password: user.password,
+            guid: user.guid,
+          };
+
+          const accessToken = jwt.sign(encryptedUser, ACCESS_SECRET_TOKEN);
+          return { accessToken: accessToken };
+        } else {
+          return null;
+        }
+      } catch (err) {
+        console.log(err);
+        return null;
+      }
+    },
+  },
+  Mutation: {
+    async createUser(_, args) {
+      const newUser = new User();
+      try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(args.input.password, salt);
+
+        newUser.password = hashedPassword;
+        newUser.username = args.input.username;
+
+        newUser.save();
+        console.log(await User.find());
+
+        return newUser;
+      } catch (error) {
+        console.log('error', error);
+        return newUser;
+      }
     },
   },
 };
