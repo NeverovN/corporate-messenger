3acca26acb03258a436b796d75c94125a45349d5
NeverovN <neverovvnn@gmail.com>
2021-06-10 09:16:57 +0300

    constant implemented

--- mobile/src/chat/chatList/hooks/useOnChatPressed.ts
+++ mobile/src/chat/chatList/hooks/useOnChatPressed.ts
@@ -5,6 +5,7 @@ import { SharedStackNavigationProp } from '@/app/types/routes';
 
 // routers
 import { SHARED_STACK_NAME, CHAT_STACK_NAME } from 'app/constants/routes';
+import { CHAT_SCREEN_NAME } from 'chat/chatList/constants/routes';
 
 export const useOnChatPressed = (chatId: string) => {
   const navigation = useNavigation<SharedStackNavigationProp>();
@@ -12,7 +13,7 @@ export const useOnChatPressed = (chatId: string) => {
   return () => {
     navigation.navigate(SHARED_STACK_NAME, {
       screen: CHAT_STACK_NAME,
-      params: { screen: 'Chat', params: { chatId } },
+      params: { screen: CHAT_SCREEN_NAME, params: { chatId } },
     });
   };
 };
