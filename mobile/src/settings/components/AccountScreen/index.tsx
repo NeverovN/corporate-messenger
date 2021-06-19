828231b996fcce8e36b7908e1be3eb465b3d15a9
NeverovN <57796735+NeverovN@users.noreply.github.com>
2021-06-19 19:32:56 +0300

    Update mobile/src/settings/components/AccountScreen/index.tsx
    Co-authored-by: Anton Patrushev <43994663+phantom-patrick@users.noreply.github.com>

--- mobile/src/settings/components/AccountScreen/index.tsx
+++ mobile/src/settings/components/AccountScreen/index.tsx
@@ -16,9 +16,9 @@ const AccountScreenView: FC<IAccountScreenViewProps> = () => {
   return (
     <View style={styles.accountScreenStyle}>
       <Text style={styles.textStyle}>Edit user account information menu</Text>
-      <EditButton screen={Directions.EMAIL} title={'Edit email'} />
-      <EditButton screen={Directions.PASSWORD} title={'Edit password'} />
-      <EditButton screen={Directions.USERNAME} title={'Edit username'} />
+      <EditButton screen={Directions.EMAIL} title="Edit email" />
+      <EditButton screen={Directions.PASSWORD} title="Edit password" />
+      <EditButton screen={Directions.USERNAME} title="Edit username" />
     </View>
   );
 };
