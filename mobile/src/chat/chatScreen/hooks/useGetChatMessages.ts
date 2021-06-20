27294b4a9254e5427fa4b329ff5673f7f8e1d20b
NeverovN <neverovvnn@gmail.com>
2021-06-21 00:40:18 +0300

    subscription cache update

--- mobile/src/chat/chatScreen/hooks/useGetChatMessages.ts
+++ mobile/src/chat/chatScreen/hooks/useGetChatMessages.ts
@@ -1,7 +1,9 @@
 import {
   useNewMessageSubscription,
   useGetChatByIdQuery,
+  MessageFragmentFragmentDoc,
 } from '@/common/types/gql.generated';
+import { useApolloClient } from '@apollo/client';
 import { useRoute } from '@react-navigation/native';
 
 // types
@@ -9,6 +11,8 @@ import { ChatRouteProp } from '../../chatList/types/routes';
 
 export const useGetChatMessages = () => {
   const { params } = useRoute<ChatRouteProp>();
+
+  const client = useApolloClient();
 
   if (!params) {
     throw Error('chat does not exist');
@@ -20,6 +24,28 @@ export const useGetChatMessages = () => {
 
   useNewMessageSubscription({
     variables: { chatId: params.chatId },
+    onSubscriptionData: ({ subscriptionData }) => {
+      const message = subscriptionData.data?.newMessage;
+
+      if (!message) {
+        throw Error('update error');
+      }
+
+      client.cache.modify({
+        fields: {
+          getChatById() {
+            try {
+              client.cache.writeFragment({
+                fragment: MessageFragmentFragmentDoc,
+                data: message,
+              });
+            } catch (err) {
+              throw Error(`cache update error -> ${err}`);
+            }
+          },
+        },
+      });
+    },
   });
 
   if (!data || !data.getChatById || !data.getChatById.messages) {
