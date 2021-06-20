0a7ee90fc14d7a8eedfb7577ea0ac356b39f82ce
NeverovN <57796735+NeverovN@users.noreply.github.com>
2021-06-21 02:04:03 +0300

    Update mobile/src/settings/types/user.ts
    Co-authored-by: Anton Patrushev <43994663+phantom-patrick@users.noreply.github.com>

--- mobile/src/settings/types/user.ts
+++ mobile/src/settings/types/user.ts
@@ -1,11 +1,8 @@
-interface IUserWithoutFriends {
+export interface IUser {
   id: string;
   email: string;
   firstName: string;
   lastName: string;
   avatar: string;
-}
-
-export interface IUser extends IUserWithoutFriends {
-  friends: Array<any>;
+  friends: Array<string>
 }
