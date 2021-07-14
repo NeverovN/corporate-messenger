b85030396b638fdd959d0a0ea6ec0dd8aa359f9b
NeverovN <57796735+NeverovN@users.noreply.github.com>
2021-07-14 03:17:28 +0300

    fixed flatlist ux (#56)

--- mobile/src/profile/components/Profile/styles.ts
+++ mobile/src/profile/components/Profile/styles.ts
@@ -2,7 +2,10 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   profileStyle: {
-    flex: 1,
+    // flex: 1,
     marginHorizontal: 5,
+  },
+  flatListStyle: {
+    flexDirection: 'column-reverse',
   },
 });
