7cbad8b5396d000dab0294379011662b8000a751
NeverovN <neverovvnn@gmail.com>
2021-07-14 06:09:44 +0300

    data security

--- mobile/src/chat/chatList/utils/getLastItem.ts
+++ mobile/src/chat/chatList/utils/getLastItem.ts
@@ -2,5 +2,7 @@ export function getLastItem<T>(arr: T[] | null | undefined) {
   if (typeof arr === 'undefined' || arr === null) {
     return undefined;
   }
-  return arr.slice(-1).pop();
+
+  const newArr = [...arr];
+  return newArr.slice(-1).pop();
 }
