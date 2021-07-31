7a60fedbce3cc7c8baf5c8d04fe9ed4771d320cc
NeverovN <neverovvnn@gmail.com>
2021-07-31 17:23:57 +0300

    chat tile fixed

--- mobile/src/chat/chatList/components/ChatInfo/index.tsx
+++ mobile/src/chat/chatList/components/ChatInfo/index.tsx
@@ -3,18 +3,18 @@ import { View, Text } from 'react-native';
 
 import styles from './styles';
 
-interface IUserInfoViewProps {
-  userName: string;
+interface IChatInfoViewProps {
+  chatTitle: string;
   lastMessage: string; // string for now
 }
 
-const UserInfoView: FC<IUserInfoViewProps> = (props) => {
+const ChatInfoView: FC<IChatInfoViewProps> = (props) => {
   return (
     <View style={styles.chatInfoViewStyle}>
-      <Text style={styles.primaryTextStyle}>{props.userName} </Text>
+      <Text style={styles.primaryTextStyle}>{props.chatTitle} </Text>
       <Text style={styles.secondaryTextStyle}>{props.lastMessage}</Text>
     </View>
   );
 };
 
-export default memo(UserInfoView);
+export default memo(ChatInfoView);
--- mobile/src/chat/chatList/utils/setLastMessage.ts
+++ mobile/src/chat/chatList/utils/setLastMessage.ts
@@ -1,7 +1,18 @@
 import { GetChatByIdQueryResult } from 'common/types/gql.generated';
 
 export const setLastMessage = (chat: GetChatByIdQueryResult) => {
-  return (
-    chat.data?.getChatById?.messages?.find(() => true)?.content.text || 'photo'
-  );
+  if (chat.data?.getChatById?.messages?.length === 0) {
+    return 'empty';
+  }
+  const lastMsg = chat.data?.getChatById?.messages?.find(() => true)?.content
+    .text;
+  if (!lastMsg) {
+    return 'photo';
+  }
+
+  if (lastMsg.length > 30) {
+    return `${lastMsg.slice(0, 30)}...`;
+  }
+
+  return lastMsg;
 };
