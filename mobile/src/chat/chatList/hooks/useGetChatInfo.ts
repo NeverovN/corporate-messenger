8f59fe7a4ab96670a6b2438ebbd82a30adfcfd09
NeverovN <57796735+NeverovN@users.noreply.github.com>
2021-08-06 17:51:08 +0300

    Dialog title & logo (#77)
    * added dialog title and logo based on second user name and avatar
    
    * redundand import change fixed
    
    * plist added

--- mobile/src/chat/chatList/hooks/useGetChatInfo.ts
+++ mobile/src/chat/chatList/hooks/useGetChatInfo.ts
@@ -2,11 +2,12 @@ import { useGetChatByIdQuery } from '@/common/types/gql.generated';
 
 // utils
 import { setLastMessage } from 'chat/chatList/utils/setLastMessage';
+import { getChatTitle } from '../utils/getChatTitle';
 
 export const useGetChatInfo = (chatId: string) => {
   const chatResult = useGetChatByIdQuery({ variables: { chatId } });
 
-  const title = chatResult.data?.getChatById?.title;
+  const title = getChatTitle(chatResult);
   const lastMsg = setLastMessage(chatResult);
 
   return {
