d80c5115e0c245357736d98fb7ad9087a04a4cac
NeverovN <57796735+NeverovN@users.noreply.github.com>
2021-08-18 09:22:52 +0300

    navigation animation fixed (#80)

--- mobile/src/feed/hooks/useUserRedirection.ts
+++ mobile/src/feed/hooks/useUserRedirection.ts
@@ -1,20 +1,13 @@
-import { useGetUserByIdQuery } from '@/common/types/gql.generated';
-import { getUsername } from '@/profile/utils/getUsername';
 import { useNavigation } from '@react-navigation/native';
 import { ProfileStackNavigationProp } from 'app/types/routes';
 
 export const useUserRedirection = (userId: string) => {
   const navigation = useNavigation<ProfileStackNavigationProp>();
-  const { data } = useGetUserByIdQuery({ variables: { id: userId } });
 
-  const username = getUsername(
-    data?.getUserById?.firstName || '',
-    data?.getUserById?.lastName || '',
-  );
   return () => {
     navigation.navigate('ProfileStack', {
       screen: 'ThirdPartyUser',
-      params: { userId, username },
+      params: { userId },
     });
   };
 };
